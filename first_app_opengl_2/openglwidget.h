bf35c30fe591972f9d2cc7b33a47ba634b9c8cb7
Walter Oliveira <32906277+walteroliveira@users.noreply.github.com>
2022-10-11 11:27:01 -0300

    Add files via upload

---
+++ first_app_opengl_2/openglwidget.h
@@ -0,0 +1,42 @@
+#ifndef OPENGLWIDGET_H
+#define OPENGLWIDGET_H
+
+#include <QOpenGLWidget>
+#include <QOpenGLFunctions_4_0_Core>
+#include <QVector4D>
+#include <vector>
+#include <QFile>
+
+class OpenGLWidget : public QOpenGLWidget, public QOpenGLFunctions_4_0_Core
+{
+    Q_OBJECT
+public:
+    explicit OpenGLWidget(QWidget *parent = nullptr);
+    ~OpenGLWidget();
+
+    GLuint vboVertices{0};
+    GLuint vboColors{0};
+    GLuint eboIndices{0};
+    GLuint vao{0};
+
+    std::vector<QVector4D> vertices;
+    std::vector<QVector4D> colors;
+    std::vector<GLuint> indices;
+
+    GLuint shaderProgram{0};
+
+    void createVBOs();
+    void createShaders();
+    void destroyVBOs();
+    void destroyShaders();
+    void changeDiagonal();
+
+protected:
+    void initializeGL() ;
+    void resizeGL(int w, int h);
+    void paintGL();
+
+public slots:
+    void toggleDarkMode(bool);
+};
+#endif // OPENGLWIDGET_H
