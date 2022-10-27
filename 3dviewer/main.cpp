dbdef86f5d330b27a89a52c8d93bac94f50c43fa
Walter Oliveira <32906277+walteroliveira@users.noreply.github.com>
2022-10-27 09:24:18 -0300

    final3d

--- 3dviewer/main.cpp
+++ 3dviewer/main.cpp
@@ -1,11 +1,25 @@
-#include "mainwindow.h"
-
 #include <QApplication>
+#include <QSurfaceFormat>
+#include "mainwindow.h"
 
 int main(int argc, char *argv[])
 {
+    QSurfaceFormat format;
+
+    #ifdef Q_OS_MAC
+        format.setVersion(4, 1);
+    #else
+        format.setVersion(4, 0);
+    #endif
+
+    format.setProfile(QSurfaceFormat::CoreProfile);
+    format.setDepthBufferSize(24);
+    format.setSamples(4);
+    QSurfaceFormat::setDefaultFormat(format);
     QApplication a(argc, argv);
+
     MainWindow w;
     w.show();
+
     return a.exec();
 }
